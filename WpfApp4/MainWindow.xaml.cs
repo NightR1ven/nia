5158c674652da157fad9ee69834de84822dca446
arbuz kirill <arbuzov.k@bk.ru>
2020-04-28 23:04:42 +0300

    Добавьте файлы проекта.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.models;
+using System.Data.Entity;
+using WpfApp4.pages;
+
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KupiProdaiDBEntities db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            try
+            {
+                db = new KupiProdaiDBEntities();
+                db.tWorker.Load();
+
+                dttWorker.ItemsSource = db.tWorker.Local.ToBindingList();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+                db.Dispose();
+            }
+
+        }
+
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+                db.Dispose();
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dttWorker.SelectedItems.Count > 0)
+                {
+                    for (int i = 0; i < dttWorker.SelectedItems.Count; i++)
+                    {
+                        tWorker worker = dttWorker.SelectedItems[i] as tWorker;
+                        if (worker != null)
+                        {
+                            db.tWorker.Remove(worker);
+                        }
+                    }
+                }
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка");
+                db.Dispose();
+                LoadData();
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (dttWorker.SelectedItems.Count > 0)
+            {
+                tWorker worker = dttWorker.SelectedItem as tWorker;
+                if (worker == null)
+                    return;
+                WindowEdit windowEdit = new WindowEdit(worker, db);
+                windowEdit.ShowDialog();
+                db.Dispose();
+                LoadData();
+
+            }
+
+        }
+    }
+}
---
+++ WpfApp4/pages/WindowEdit.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp4.models;
+
+namespace WpfApp4.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowEdit.xaml
+    /// </summary>
+    public partial class WindowEdit : Window
+    {
+        KupiProdaiDBEntities db;
+        tWorker sp;
+        public WindowEdit()
+        {
+            InitializeComponent();
+        }
+
+        public WindowEdit(tWorker worker,KupiProdaiDBEntities d)
+        {
+            InitializeComponent();
+            db = d;
+            sp = worker;
+            tbFIO.Text = worker.sFIO;
+            txAddress.Text = worker.address;
+            dtpBithday.SelectedDate = worker.bithday;
+            tbphone.Text = worker.phone;
+            tbbase.Text = worker.base_worker;
+            tbexp.Text = Convert.ToString(worker.experience);
+            tbsp.Text = worker.special;
+            tbprice.Text = Convert.ToString(worker.price);
+            tbpas.Text = Convert.ToString(worker.passport);
+
+
+        }
+
+        private void BtnSAve_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            sp.sFIO = tbFIO.Text;
+            sp.passport = tbpas.Text;
+            sp.address = txAddress.Text;
+            
+
+            db.SaveChanges();
+        }
+    }
+}
